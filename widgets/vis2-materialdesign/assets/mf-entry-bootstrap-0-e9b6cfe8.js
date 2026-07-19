d0e328c6e4ba4408b1bba9d45af149c698522aa5
typhosj <mrennecke@gmx.de>
2026-07-19 21:19:26 +0200

    fix: list base layout

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-e9b6cfe8.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-e9b6cfe8.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-CCWXzJI6.js"));
+})().then(() => __mfImport("./index-CKFN9dUe.js"));
