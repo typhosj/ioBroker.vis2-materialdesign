f0a117ca966640bcaa3857f56245e4ebebb58a56
typhosj <mrennecke@gmx.de>
2026-07-24 11:17:10 +0200

    chore(m3): rebuild widget bundle for Phase 4 slices 2-3
    Regenerated widgets/ and admin/ build output for the List/Icon List/Views M3 changes.

--- widgets/vis2-materialdesign/customWidgets.js
+++ widgets/vis2-materialdesign/customWidgets.js
@@ -1,1 +1,1 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesignWidgets__customWidgets_js-DSygmoHd.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesignWidgets__customWidgets_js-DLCe2LK2.js";export{t as get,e as init};
