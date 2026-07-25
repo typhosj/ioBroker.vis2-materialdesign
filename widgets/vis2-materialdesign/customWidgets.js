c3a20c5423d600ca506d7b69dcd78d9e4ef49ab8
typhosj <mrennecke@gmx.de>
2026-07-25 21:03:31 +0200

    chore(m3): rebuild widget bundle for Phase 7 slice 3 + Phase 8 slice 1

--- widgets/vis2-materialdesign/customWidgets.js
+++ widgets/vis2-materialdesign/customWidgets.js
@@ -1,1 +1,1 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesignWidgets__customWidgets_js-ByKas_eO.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesignWidgets__customWidgets_js-BCV0nxFT.js";export{t as get,e as init};
