9887af4d6b1c2cb21a00e23253af7dd54da5eb34
typhosj <mrennecke@gmx.de>
2026-07-26 20:31:23 +0200

    chore(charts): rebuild widget bundle for the value-label fixes

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-7e89a889.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-7e89a889.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-92zYs9h8.js"));
+})().then(() => __mfImport("./index-CZKZGmbR.js"));
