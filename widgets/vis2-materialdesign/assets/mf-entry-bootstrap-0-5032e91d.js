cc6a7a4e286964afa82138241c5b7661dc1e649a
typhosj <typhosj@gmx.de>
2026-08-15 11:39:47 +0200

    chore(build): rebuild widget bundle with the advanced view fix

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-5032e91d.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-5032e91d.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-Cnh5p37r.js"));
+})().then(() => __mfImport("./index-DxMdAsFt.js"));
