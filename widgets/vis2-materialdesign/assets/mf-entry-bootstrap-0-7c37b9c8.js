eabc5d73a3732b89d3173423f12d480023de9f5d
typhosj <typhosj@gmx.de>
2026-08-16 11:34:04 +0200

    chore(build): rebuild widget bundle with the calendar fixes

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-7c37b9c8.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-7c37b9c8.js
@@ -33,4 +33,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-C2oBSJAZ.js"));
+})().then(() => __mfImport("./index-DXf2o-WE.js"));
---
+++ widgets/vis2-materialdesign/assets/virtualExposes-DsPQf7nG.js
@@ -0,0 +1,1 @@
+import{t as e}from"./virtual_mf-exposes___mfe_internal__vis2MaterialDesignWidgets-DdKo-vyF.js";export{e as default};
