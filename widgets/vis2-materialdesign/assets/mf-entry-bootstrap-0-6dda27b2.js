21aee58434aeba6eb26e47488803cdafbad09f31
typhosj <typhosj@gmx.de>
2026-08-16 12:12:55 +0200

    chore(build): rebuild widget bundle with the calendar features

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-6dda27b2.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-6dda27b2.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-DChr5L_l.js"));
+})().then(() => __mfImport("./index-BGTfzTaN.js"));
