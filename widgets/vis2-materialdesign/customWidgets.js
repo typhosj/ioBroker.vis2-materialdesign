7607556b37f307492e2b5f5f4483cf4f07461112
typhosj <typhosj@gmx.de>
2026-08-16 10:01:18 +0200

    chore(build): rebuild widget bundle with the addition clamp fix

--- widgets/vis2-materialdesign/customWidgets.js
+++ widgets/vis2-materialdesign/customWidgets.js
@@ -1,1 +1,1 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesig-CbRWNz0e.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesig-CmPsqCNo.js";export{t as get,e as init};
