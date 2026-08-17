3c361732afa44a16f97be385064dc3c1ff8bda58
typhosj <typhosj@gmx.de>
2026-08-17 08:01:10 +0200

    chore(build): rebuild widget bundle with the calendar event placement

--- widgets/vis2-materialdesign/customWidgets.js
+++ widgets/vis2-materialdesign/customWidgets.js
@@ -1,1 +1,1 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesig-Ct4TgCZh.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__vis2MaterialDesig-C-HnKEEh.js";export{t as get,e as init};
