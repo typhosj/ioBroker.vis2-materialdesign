977c43522affdd8139ea62bb1c8d85dfba77b6f3
typhosj <typhosj@gmx.de>
2026-08-17 06:53:36 +0200

    chore(build): rebuild widget bundle with the app bar click fix

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-3c49ef72.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-3c49ef72.js
@@ -33,4 +33,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-DbctXXnw.js"));
+})().then(() => __mfImport("./index-DY5XfGgJ.js"));
