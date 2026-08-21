7e577103489f4f074c1b5ebeeaebce6db0aca020
typhosj <typhosj@gmx.de>
2026-08-21 07:10:32 +0200

    chore(build): rebuild the widget bundles

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-fd030283.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-fd030283.js
@@ -32,4 +32,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-CKETeR9A.js"));
+})().then(() => __mfImport("./index-BZuurc5K.js"));
--- widgets/vis2-materialdesign/assets/virtualExposes-CXuvZHrd.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./virtual_mf-exposes___mfe_internal__vis2MaterialDesignWidgets-PywgKO7h.js";export{e as default};
