08e4be9c4bb691e0ead818a727448e6e19edd7cf
typhosj <typhosj@gmx.de>
2026-08-21 07:04:38 +0200

    chore(build): rebuild the widget bundles

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-820585ec.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-820585ec.js
@@ -33,4 +33,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-DnZDX3yn.js"));
+})().then(() => __mfImport("./index-DSqiEmnu.js"));
