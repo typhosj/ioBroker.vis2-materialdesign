d7d3136181183b87fc6bc5122a5f3d29952e7055
typhosj <typhosj@gmx.de>
2026-09-01 09:31:45 +0200

    chore(build): rebuild the widget bundles
    Carries the JSON chart value labels into widgets/. Only the five chart chunks
    and the entries that reference them changed hash.

--- widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-8881d39a.js
+++ widgets/vis2-materialdesign/assets/mf-entry-bootstrap-0-8881d39a.js
@@ -33,4 +33,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-D0cOVt6s.js"));
+})().then(() => __mfImport("./index-HoYcPip5.js"));
