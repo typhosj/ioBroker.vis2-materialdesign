dbaf6c576b44e427ccf1f15fd130795877f78d9d
typhosj <typhosj@gmx.de>
2026-09-04 09:00:44 +0200

    chore(build): rebuild the widget bundles

---
+++ widgets/vis2-materialdesign/assets/MaterialDesignCheckbox-BBkdXMIK.js
@@ -0,0 +1,1 @@
+import{t as e}from"./MaterialDesignToggleControls-DaOn6Ryw.js";var t=e({id:`tplVis2-materialdesign-CheckBox`,name:`Checkbox`,kind:`checkbox`});export{t as default};
--- widgets/vis2-materialdesign/assets/virtualExposes-D70amvj_.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./virtual_mf-exposes___mfe_internal__vis2MaterialDesignWidgets-Do1Y5vMR.js";export{e as default};
