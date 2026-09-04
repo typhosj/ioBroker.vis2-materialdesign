1265f4f09c2c658f04da8bc8f7753c09d9a5e4d5
typhosj <typhosj@gmx.de>
2026-09-04 09:24:54 +0200

    chore(build): rebuild the widget bundles

---
+++ widgets/vis2-materialdesign/assets/MaterialDesignSwitch-BXmQKYQQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./MaterialDesignToggleControls-aKX6ay0D.js";var t=e({id:`tplVis2-materialdesign-Switch`,name:`Switch`,kind:`switch`});export{t as default};
--- widgets/vis2-materialdesign/assets/MaterialDesignSwitch-xgXA8VgJ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./MaterialDesignToggleControls-DaOn6Ryw.js";var t=e({id:`tplVis2-materialdesign-Switch`,name:`Switch`,kind:`switch`});export{t as default};
